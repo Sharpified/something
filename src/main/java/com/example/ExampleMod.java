c8180fed88b99273b3560101757818c3bd571d62
Sharpified <seven459750@gmail.com>
2026-03-28 20:16:36 +0530

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,24 +1,50 @@
-package com.example;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ExampleMod implements ModInitializer, ClientModInitializer {
+    private int ticks = 0;
+    private boolean pressed = false;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+    @Override
+    public void onInitialize() {
+        System.out.println("PvP Mod Loaded!");
+    }
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    @Override
+    public void onInitializeClient() {
+        KeyBinding attackG = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.attack2", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_G, "PvP Binds"));
+        
+        KeyBinding bucketQ = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.bucket", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Q, "PvP Binds"));
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.player == null) return;
 
-		LOGGER.info("Hello Fabric world!");
-	}
+            if (attackG.isPressed()) {
+                client.options.attackKey.setPressed(true);
+            }
+
+            if (bucketQ.isPressed()) {
+                ticks++;
+                pressed = true;
+                if (ticks > 5) {
+                    client.player.getInventory().selectedSlot = 4; 
+                }
+            } else if (pressed) {
+                if (ticks <= 5) {
+                    client.player.getInventory().selectedSlot = 3; 
+                }
+                ticks = 0;
+                pressed = false;
+            }
+        });
+    }
 }
